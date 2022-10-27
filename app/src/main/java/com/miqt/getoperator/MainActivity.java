b907b513fd3e5773c2f1c781e146c210e1ab7fd7
xxx <xxx>
2022-10-27 17:02:12 +0800

    init

---
+++ app/src/main/java/com/miqt/getoperator/MainActivity.java
@@ -0,0 +1,18 @@
+package com.miqt.getoperator;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView view = findViewById(R.id.text);
+        view.setText(getString(R.string.operator));
+    }
+}
